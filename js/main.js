74c7822e1a51baf030d933404865a6fceb6272d6
rein-viewec <83944723+rein-viewec@users.noreply.github.com>
2021-10-27 17:07:10 +0800

    finish all pages

--- js/main.js
+++ js/main.js
@@ -1,39 +1,44 @@
 
+const tabSetting = {
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  grabCursor: true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+}
+const tabItemSetting = {
+  slidesPerView: 2,
+  spaceBetween: 0,
+  grabCursor: true,
+  breakpoints: {
+    '720': {
+      slidesPerView: 'auto',
+      spaceBetween: 0,
+    },
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+}
 $(document).ready(() => {
+  $('#Sidebar').load('sidebar.html')
+  $('#Footer').load('footer.html')
   $('.item__tab__new').tab()
   $('.item__tab__hot').tab()
   AOS.init({
     once: true,
   })
-  const tabSetting = {
-    slidesPerView: 'auto',
-    spaceBetween: 0,
-    grabCursor: true,
-  }
-  const tabItemSetting = {
-    slidesPerView: 'auto',
-    spaceBetween: 0,
-    grabCursor: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  }
   new Swiper('.item__tab__new', tabSetting)
   new Swiper('.tab__content', tabItemSetting)
-  // Sidebar toggle
-  $('.category__expension__title').on('click', function () {
-    $(this)
-      .toggleClass('active')
-      .next('.category__secondary')
-      .slideToggle('fast')
-  })
 })
